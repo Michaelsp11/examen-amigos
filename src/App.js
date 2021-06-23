da3e1d6c535803ade7414f837bccc315c4930262
Michael Salgado <michaelsalgadop11@gmail.com>
2021-06-23 09:29:31 +0200

    Estructura HTML con Bootstrap principal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,67 @@
 function App() {
   return (
-    <p>Hola</p>
+    <>
+      <header className="container">
+        <h1>Gestión de mis 6 amigos</h1>
+        <button type="button" className="boton btn">
+          Crear amigo
+        </button>
+      </header>
+      <main className="contenedor-principal container">
+        <ul>
+          <li>
+            <i></i>
+            <i></i>
+            <p>Nombre: Juan</p>
+            <p>Apellido: González</p>
+            <p>
+              Valoración: <i></i>
+            </p>
+          </li>
+          <li>
+            <i></i>
+            <i></i>
+            <p>Nombre: Juan</p>
+            <p>Apellido: González</p>
+            <p>
+              Valoración: <i></i>
+            </p>
+          </li>
+          <li>
+            <p>Nombre: Juan</p>
+            <p>Apellido: González</p>
+            <p>
+              Valoración: <i></i>
+            </p>
+          </li>
+          <li>
+            <p>Nombre: Juan</p>
+            <p>Apellido: González</p>
+            <p>
+              Valoración: <i></i>
+            </p>
+          </li>
+          <li>
+            <i></i>
+            <i></i>
+            <p>Nombre: Juan</p>
+            <p>Apellido: González</p>
+            <p>
+              Valoración: <i></i>
+            </p>
+          </li>
+          <li>
+            <i></i>
+            <i></i>
+            <p>Nombre: Juan</p>
+            <p>Apellido: González</p>
+            <p>
+              Valoración: <i></i>
+            </p>
+          </li>
+        </ul>
+      </main>
+    </>
   );
 }
 
